b8fa0f32420632053340a7e570427b1d8252b8c3
Ingimar Erlingsson <inkimar@gmail.com>
2018-08-08 13:47:17 +0200

    Replacing function fetchfile with 2 files, only one responsibility per function

--- js/5-todo-api/my-todo/src/todos.js
+++ js/5-todo-api/my-todo/src/todos.js
@@ -24,7 +24,7 @@ app.use(expressValidator())
 const log = debug('DINA-SCHOOL:server')
 const nextTodoId = 5
 
-function validate(req, res, next) {
+function completeInputValidate(req, res, next) {
   req.checkBody('todo', 'invalid todo').notEmpty()
   req.checkBody('done', 'invalid done').isBoolean()
   req.checkBody('date', 'invalid date').notEmpty()
@@ -42,15 +42,25 @@ function validate(req, res, next) {
   return next()
 }
 
+/*
 function fetchFile(req, res, next) {
   // decorateStoredTodos, decorateStoredTodosMiddleware
   const file = readFileSync('todos.json', 'utf8')
   res.locals.todos = JSON.parse(file)
   return next()
 }
+*/
+function readTodosFromFile() {
+  const file = readFileSync('todos.json', 'utf8')
+  return JSON.parse(file)
+}
+
+function decorateStoredTodosMiddleware(req, res, next) {
+  res.locals.todos = readTodosFromFile()
+  return next()
+}
 
 function writeToFile(chunk) {
-  // saveTodos ...
   const json = JSON.stringify(chunk)
   writeFile('todos.json', json, err => {
     if (err) log(`Failed to write file: ${err}`)
@@ -86,20 +96,13 @@ function checking(req, res, next) {
   }
   return next()
 }
-/* @Todo - inte skriva på svenska ...
-obs: första argument, variablen 'id' ska överenstämma med var i andra REST-sökvägar
- hanterar dessa tre:  
- app.put('/todos/:id'
- app.delete('/todos/:id'
- app.get('/todos/'
-*/
 
-app.get('/todos/', fetchFile, (req, res) => {
+app.get('/todos/', decorateStoredTodosMiddleware, (req, res) => {
   const { todos } = res.locals
   res.send(todos)
 })
 
-app.get('/todos/:id', fetchFile, checking, (req, res) => {
+app.get('/todos/:id', decorateStoredTodosMiddleware, checking, (req, res) => {
   res.send(req.todo)
 })
 
@@ -111,21 +114,26 @@ app.get('/todos/:id', fetchFile, checking, (req, res) => {
         "date": "2018-08-11"
  }
 */
-app.post('/todos/', fetchFile, validate, (req, res) => {
-  const { todos } = res.locals
-  log(todos)
-  const newTodo = req.body
-  newTodo.id = nextTodoId + 1
-  todos.push(newTodo)
-  writeToFile(todos)
-  res.status(201).send(newTodo)
-})
+app.post(
+  '/todos/',
+  decorateStoredTodosMiddleware,
+  completeInputValidate,
+  (req, res) => {
+    const { todos } = res.locals
+    log(todos)
+    const newTodo = req.body
+    newTodo.id = nextTodoId + 1
+    todos.push(newTodo)
+    writeToFile(todos)
+    res.status(201).send(newTodo)
+  }
+)
 
 /*
  {"id": 5,"done":false, "todo":"walk 22 miles","date":"2018-07-25"}
  const { storedTodo, todos } = res.locals
 */
-app.put('/todos/:id', fetchFile, checking, (req, res) => {
+app.put('/todos/:id', decorateStoredTodosMiddleware, checking, (req, res) => {
   const { todos } = res.locals
   const updatedTodo = req.body
 
@@ -140,7 +148,7 @@ app.put('/todos/:id', fetchFile, checking, (req, res) => {
 /*
 update only one attribute at a time, for instance '{"done": true}'
 */
-app.patch('/todos/:id', fetchFile, checking, (req, res) => {
+app.patch('/todos/:id', decorateStoredTodosMiddleware, checking, (req, res) => {
   const { todos } = res.locals // 8 aug. har denna reda denna, har - se checking ...
   const chosenTodo = todos[req.todoIndex] // 8 aug. se ovan ....
 
@@ -163,12 +171,17 @@ app.patch('/todos/:id', fetchFile, checking, (req, res) => {
   res.status(201).send(chosenTodo) // ska hela returneras ?
 })
 
-app.delete('/todos/:id', fetchFile, checking, (req, res) => {
-  const { todos } = res.locals
-  todos.splice(req.todoIndex, 1)
-  writeToFile(todos)
-  res.status(204).send()
-})
+app.delete(
+  '/todos/:id',
+  decorateStoredTodosMiddleware,
+  checking,
+  (req, res) => {
+    const { todos } = res.locals
+    todos.splice(req.todoIndex, 1)
+    writeToFile(todos)
+    res.status(204).send()
+  }
+)
 
 app.listen(PORT, () => {
   log(`Server is listening on port ${PORT}`)
