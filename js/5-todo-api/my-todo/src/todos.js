bd5a89f33ef03a507d3f1cd81fb23154434ca39b
Ingimar Erlingsson <inkimar@gmail.com>
2018-08-08 14:18:11 +0200

    Rename function checking -> decorateStoredTodo

--- js/5-todo-api/my-todo/src/todos.js
+++ js/5-todo-api/my-todo/src/todos.js
@@ -61,7 +61,7 @@ function saveTodos(chunk) {
   })
 }
 
-function checking(req, res, next) {
+function decorateStoredTodo(req, res, next) {
   const { todos } = res.locals
   const idToFind = Number(req.params.id)
   const rackIndex = todos.findIndex(todo => todo.id === idToFind)
@@ -76,13 +76,19 @@ function checking(req, res, next) {
 }
 
 app.get('/todos/', decorateStoredTodosMiddleware, (req, res) => {
+  log(`get is called${nextTodoId}`)
   const { todos } = res.locals
   res.send(todos)
 })
 
-app.get('/todos/:id', decorateStoredTodosMiddleware, checking, (req, res) => {
-  res.send(req.todo)
-})
+app.get(
+  '/todos/:id',
+  decorateStoredTodosMiddleware,
+  decorateStoredTodo,
+  (req, res) => {
+    res.send(req.todo)
+  }
+)
 
 /*
   A reminder on how to create a new todo from 'postman' (choose-> POST, body+raw+JSON ) 
@@ -118,43 +124,53 @@ app.post(
       "date": "2018-08-25"
     }
 */
-app.put('/todos/:id', decorateStoredTodosMiddleware, checking, (req, res) => {
-  const { todos } = res.locals
-  const updatedTodo = req.body
+app.put(
+  '/todos/:id',
+  decorateStoredTodosMiddleware,
+  decorateStoredTodo,
+  (req, res) => {
+    const { todos } = res.locals
+    const updatedTodo = req.body
 
-  if (req.todo.id !== updatedTodo.id) {
-    res.status(400).send('Cannot update Todo Id')
-  } else {
-    todos[req.todoIndex] = updatedTodo
-    saveTodos(todos)
-    res.status(201).send(todos[req.todoIndex])
+    if (req.todo.id !== updatedTodo.id) {
+      res.status(400).send('Cannot update Todo Id')
+    } else {
+      todos[req.todoIndex] = updatedTodo
+      saveTodos(todos)
+      res.status(201).send(todos[req.todoIndex])
+    }
   }
-})
+)
 /*
   A reminder on how to patch a todo from 'postman' (choose-> PATCH, body+raw+JSON ) 
   call : http://localhost:4001/todos/6 
   only one attribute at a time: Body is below: 
     {"done": true}
 */
-app.patch('/todos/:id', decorateStoredTodosMiddleware, checking, (req, res) => {
-  const { todos } = res.locals
-  const chosenTodo = todos[req.todoIndex]
+app.patch(
+  '/todos/:id',
+  decorateStoredTodosMiddleware,
+  decorateStoredTodo,
+  (req, res) => {
+    const { todos } = res.locals
+    const chosenTodo = todos[req.todoIndex]
 
-  const { body } = req
+    const { body } = req
 
-  if (body.todo !== undefined) {
-    chosenTodo.todo = body.todo
-    todos[req.todoIndex].todo = body.todo
-  } else if (body.done !== undefined) {
-    chosenTodo.done = body.done
-    todos[req.todoIndex].done = body.done
-  } else if (body.date !== undefined) {
-    chosenTodo.date = body.date
-    todos[req.todoIndex].todo = body.date
+    if (body.todo !== undefined) {
+      chosenTodo.todo = body.todo
+      todos[req.todoIndex].todo = body.todo
+    } else if (body.done !== undefined) {
+      chosenTodo.done = body.done
+      todos[req.todoIndex].done = body.done
+    } else if (body.date !== undefined) {
+      chosenTodo.date = body.date
+      todos[req.todoIndex].todo = body.date
+    }
+    saveTodos(todos)
+    res.status(201).send(chosenTodo)
   }
-  saveTodos(todos)
-  res.status(201).send(chosenTodo)
-})
+)
 
 app.delete(
   '/todos/:id',
